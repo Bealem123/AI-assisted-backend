2b423c406dbbf13becf7d0f1a0dc3d5936244909
Bealem123 <sintayehubealem411@gmail.com>
2025-07-08 09:06:09 -0700

    Update Body4Page.js

--- frontend/src/pages/Body4Page.js
+++ frontend/src/pages/Body4Page.js
@@ -1,10 +1,7 @@
 import React from "react";
 import Body4 from "../components/Layout/Body4"; // Import the Body4 component
-import { useSidebar } from "../context/SidebarContext"; // Sidebar Context
 
 const Body4Page = () => {
-  const { sidebarState } = useSidebar(); // Assuming you're using the Sidebar context here
-
   return (
     <div>
       <Body4 /> {/* Render Body4 component */}
@@ -13,3 +10,4 @@ const Body4Page = () => {
 };
 
 export default Body4Page;
+
